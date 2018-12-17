b4bf1aae841a3bb8e87b112b8eb974d5e7bc3a70
jose6palabras <jose6palabras@gmail.com>
2018-12-17 14:00:04 -0500

    second commit, It includes HomeActivity images and connection login home.

---
+++ app/src/main/java/com/grupodetec/aventuraprote/View/Login/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.grupodetec.aventuraprote.View.Login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.grupodetec.aventuraprote.R;
+import com.grupodetec.aventuraprote.View.Home.HomeActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+    public void guestLogin (View view){
+        Toast.makeText(this, "Dímelo papi!", Toast.LENGTH_SHORT).show();
+        Intent intentguest = new Intent(this, HomeActivity.class);
+        startActivity(intentguest);
+
+
+    }
+}
